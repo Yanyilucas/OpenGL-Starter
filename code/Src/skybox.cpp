e455414035b0d30f82933846928aeaf254d0d04a
YaNYilucas <lizhan.yanyi@gmail.com>
2025-11-12 11:00:12 +0800

    fix: move STB_IMAGE_IMPLEMENTATION definition to skybox.cpp and remove stb_impl.cpp

--- code/Src/skybox.cpp
+++ code/Src/skybox.cpp
@@ -1,7 +1,11 @@
 #include "skybox.h"
 #include "shader.h"
 
+#ifndef STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#endif // !STB_IMAGE_IMPLEMENTATION
 
+#include "stb_image.h"
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
